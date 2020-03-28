0a038fea54d28fa73b4559dff76696db56b42f8c
omarkorbi <46576861+omarkorbi@users.noreply.github.com>
2020-03-28 23:42:44 +0100

    Update SecServiceApplication.java

--- src/main/java/org/sid/SecServiceApplication.java
+++ src/main/java/org/sid/SecServiceApplication.java
@@ -13,20 +13,23 @@ import java.util.stream.Stream;
 @SpringBootApplication
 public class SecServiceApplication {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
         SpringApplication.run(SecServiceApplication.class, args);
     }
+    
     @Bean
-    CommandLineRunner start(AccountService accountService){
+    CommandLineRunner start(AccountService accountService , AppRoleRepository roleRepo){
         return args->{
+        	AppRole r1 =roleRepo.save(new AppRole( null ,"FOURNISSEUR"));
             accountService.save(new AppRole(null,"USER"));
             accountService.save(new AppRole(null,"ADMIN"));
             Stream.of("user1","user2","user3","admin").forEach(un->{
-                accountService.saveUser(un,"1234","1234");
+                accountService.saveUser(un,"1234","1234","2121212" ,"Male" ,"monjislim");
             });
             accountService.addRoleToUser("admin","ADMIN");
         };
     }
+    
     @Bean
     BCryptPasswordEncoder getBCPE(){
         return new BCryptPasswordEncoder();
