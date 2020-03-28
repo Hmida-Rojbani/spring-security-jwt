691c563d731b8c63f18ffd53dac0e1b9705507b4
omarkorbi <46576861+omarkorbi@users.noreply.github.com>
2020-03-28 23:40:37 +0100

    Update UserController.java

--- src/main/java/org/sid/web/UserController.java
+++ src/main/java/org/sid/web/UserController.java
@@ -10,17 +10,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
-    @Autowired
-    private AccountService accountService;
-    @PostMapping("/register")
-    public AppUser register(@RequestBody  UserForm userForm){
-        return  accountService.saveUser(
-                userForm.getUsername(),userForm.getPassword(),userForm.getConfirmedPassword());
+  @Autowired
+    private AccountService accountService;//UserForm userForm
+    @PostMapping("/registerUser")
+    public AppUser registerUser(@RequestBody  UserForm userForm){
+        return  accountService.saveUser(userForm.getUsername(), userForm.getPassword(),userForm.getConfirmedPassword()
+        		    ,userForm.getPhoneNumber() ,userForm.getGender(),userForm.getAddress());
     }
+    @PostMapping("/registerFour")
+    public AppUser registerFournisseur(@RequestBody  UserForm userForm){
+        return  accountService.saveFournisseur(userForm.getUsername(), userForm.getPassword(),userForm.getConfirmedPassword()
+        		    ,userForm.getPhoneNumber() ,userForm.getGender(),userForm.getAddress());
+    }
+    
 }
+
 @Data
 class UserForm{
     private String username;
     private String password;
     private String confirmedPassword;
+    private String phoneNumber ;
+    private String gender;
+    private String address;
+    
 }
